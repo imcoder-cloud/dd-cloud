989027cc1ad613d6927b190585844e8460984c3f
dongdongchen <dd18355193335@163.com>
2020-01-17 18:42:46 +0800

    功能开发

--- db/imcoder.sql
+++ db/imcoder.sql
@@ -11,7 +11,7 @@
  Target Server Version : 80017
  File Encoding         : 65001
 
- Date: 31/12/2019 15:00:25
+ Date: 17/01/2020 18:42:19
 */
 
 SET NAMES utf8mb4;
@@ -22,17 +22,17 @@ SET FOREIGN_KEY_CHECKS = 0;
 -- ----------------------------
 DROP TABLE IF EXISTS `article`;
 CREATE TABLE `article`  (
-  `article_id` int(11) NOT NULL,
-  `article_name` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `article_id` int(11) NOT NULL AUTO_INCREMENT,
+  `title` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
   `user_id` int(11) NULL DEFAULT NULL,
-  `content` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
-  `html` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `content` longtext CHARACTER SET utf8 COLLATE utf8_general_ci NULL,
+  `html` longtext CHARACTER SET utf8 COLLATE utf8_general_ci NULL,
   `summary` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
   `status` int(11) NULL DEFAULT 1100,
   `original` tinyint(1) NULL DEFAULT NULL COMMENT '是否原创',
-  `view` int(11) NULL DEFAULT 0 COMMENT '阅读量',
-  `love` int(11) NULL DEFAULT 0 COMMENT '喜欢量',
-  `comment` int(255) NULL DEFAULT 0 COMMENT '评论量',
+  `views` int(11) NULL DEFAULT 0 COMMENT '阅读量',
+  `likes` int(11) NULL DEFAULT 0 COMMENT '喜欢量',
+  `comments` int(255) NULL DEFAULT 0 COMMENT '评论量',
   `cover` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
   `top` tinyint(1) NULL DEFAULT 0,
   `recommend` tinyint(1) NULL DEFAULT 0,
@@ -49,6 +49,31 @@ CREATE TABLE `article_category`  (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `article_id` int(11) NULL DEFAULT NULL,
   `category_id` int(11) NULL DEFAULT NULL,
+  `user_id` int(11) NULL DEFAULT NULL,
+  `create_time` timestamp(0) NULL DEFAULT CURRENT_TIMESTAMP(0),
+  `modify_time` timestamp(0) NULL DEFAULT CURRENT_TIMESTAMP(0) ON UPDATE CURRENT_TIMESTAMP(0),
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of article_category
+-- ----------------------------
+INSERT INTO `article_category` VALUES (11, 14, 6, 11, '2020-01-17 18:37:03', '2020-01-17 18:37:03');
+INSERT INTO `article_category` VALUES (12, 14, 7, 11, '2020-01-17 18:37:03', '2020-01-17 18:37:03');
+INSERT INTO `article_category` VALUES (13, 14, 8, 11, '2020-01-17 18:37:03', '2020-01-17 18:37:03');
+INSERT INTO `article_category` VALUES (14, 14, 6, 11, '2020-01-17 18:37:03', '2020-01-17 18:37:03');
+INSERT INTO `article_category` VALUES (15, 14, 7, 11, '2020-01-17 18:37:03', '2020-01-17 18:37:03');
+INSERT INTO `article_category` VALUES (16, 14, 8, 11, '2020-01-17 18:37:03', '2020-01-17 18:37:03');
+
+-- ----------------------------
+-- Table structure for article_tag
+-- ----------------------------
+DROP TABLE IF EXISTS `article_tag`;
+CREATE TABLE `article_tag`  (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `article_id` int(11) NULL DEFAULT NULL,
+  `tag_id` int(11) NULL DEFAULT NULL,
+  `user_id` int(11) NULL DEFAULT NULL,
   `create_time` timestamp(0) NULL DEFAULT CURRENT_TIMESTAMP(0),
   `modify_time` timestamp(0) NULL DEFAULT CURRENT_TIMESTAMP(0) ON UPDATE CURRENT_TIMESTAMP(0),
   PRIMARY KEY (`id`) USING BTREE
@@ -59,12 +84,18 @@ CREATE TABLE `article_category`  (
 -- ----------------------------
 DROP TABLE IF EXISTS `category`;
 CREATE TABLE `category`  (
-  `category_id` int(11) NOT NULL,
+  `category_id` int(11) NOT NULL AUTO_INCREMENT,
   `category_name` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `user_id` int(11) NULL DEFAULT NULL,
   `create_time` timestamp(0) NULL DEFAULT CURRENT_TIMESTAMP(0),
   `modify_time` timestamp(0) NULL DEFAULT CURRENT_TIMESTAMP(0) ON UPDATE CURRENT_TIMESTAMP(0),
   PRIMARY KEY (`category_id`) USING BTREE
 ) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of category
+-- ----------------------------
+INSERT INTO `category` VALUES (9, 'linux', 11, '2020-01-17 18:40:28', '2020-01-17 18:40:28');
 
 -- ----------------------------
 -- Table structure for oauth_client_details
@@ -81,11 +112,16 @@ CREATE TABLE `oauth_client_details`  (
   `access_token_validity` int(11) NULL DEFAULT NULL,
   `refresh_token_validity` int(11) NULL DEFAULT NULL,
   `additional_information` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
-  `auto_approve` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
+  `autoapprove` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
   `create_time` timestamp(0) NULL DEFAULT CURRENT_TIMESTAMP(0),
   `modify_time` timestamp(0) NULL DEFAULT CURRENT_TIMESTAMP(0) ON UPDATE CURRENT_TIMESTAMP(0),
   PRIMARY KEY (`client_id`) USING BTREE
 ) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci COMMENT = '终端信息表' ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of oauth_client_details
+-- ----------------------------
+INSERT INTO `oauth_client_details` VALUES ('client_1', NULL, '$2a$10$3hTUSGc9W2IU9PCUxqIz2e8lf810r/z0nzIgpgy1W4FZYb/SKAkFi', 'all', 'password,authorization_code,refresh_token', NULL, NULL, NULL, NULL, NULL, 'true', NULL, '2020-01-04 10:53:07');
 
 -- ----------------------------
 -- Table structure for role
@@ -97,20 +133,32 @@ CREATE TABLE `role`  (
   `create_time` timestamp(0) NULL DEFAULT CURRENT_TIMESTAMP(0),
   `modify_time` timestamp(0) NULL DEFAULT CURRENT_TIMESTAMP(0) ON UPDATE CURRENT_TIMESTAMP(0),
   PRIMARY KEY (`role_id`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB AUTO_INCREMENT = 5 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of role
+-- ----------------------------
+INSERT INTO `role` VALUES (1, 'admin1', '2019-12-31 13:14:14', '2019-12-31 13:42:20');
+INSERT INTO `role` VALUES (2, 'admin2', '2019-12-31 13:41:08', '2019-12-31 13:42:51');
 
 -- ----------------------------
 -- Table structure for tag
 -- ----------------------------
 DROP TABLE IF EXISTS `tag`;
 CREATE TABLE `tag`  (
-  `id` int(11) NOT NULL,
-  `name` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
-  `article_id` int(11) NULL DEFAULT NULL,
+  `tag_id` int(11) NOT NULL AUTO_INCREMENT,
+  `tag_name` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `user_id` int(11) NULL DEFAULT NULL,
   `create_time` timestamp(0) NULL DEFAULT CURRENT_TIMESTAMP(0),
   `modify_time` timestamp(0) NULL DEFAULT CURRENT_TIMESTAMP(0) ON UPDATE CURRENT_TIMESTAMP(0),
-  PRIMARY KEY (`id`) USING BTREE
+  PRIMARY KEY (`tag_id`) USING BTREE
 ) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of tag
+-- ----------------------------
+INSERT INTO `tag` VALUES (6, 'vue', 11, '2020-01-17 18:36:20', '2020-01-17 18:36:20');
+INSERT INTO `tag` VALUES (7, 'linux', 11, '2020-01-17 18:40:43', '2020-01-17 18:40:43');
 
 -- ----------------------------
 -- Table structure for user
@@ -119,12 +167,21 @@ DROP TABLE IF EXISTS `user`;
 CREATE TABLE `user`  (
   `user_id` int(11) NOT NULL AUTO_INCREMENT,
   `username` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
+  `mobile` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
   `email` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
   `password` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
   `create_time` timestamp(0) NULL DEFAULT CURRENT_TIMESTAMP(0),
   `modify_time` timestamp(0) NULL DEFAULT CURRENT_TIMESTAMP(0) ON UPDATE CURRENT_TIMESTAMP(0),
   PRIMARY KEY (`user_id`) USING BTREE
 ) ENGINE = InnoDB AUTO_INCREMENT = 14 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of user
+-- ----------------------------
+INSERT INTO `user` VALUES (8, 'admin', '18355193335', 'imacoder@126.com', '$2a$10$MmWjrLtYlrkfceaNafUGSu6cdAiPCTObxATlV5edj2MsE7Q4ze56u', '2019-12-04 10:17:20', '2020-01-04 11:11:17');
+INSERT INTO `user` VALUES (11, 'cdd', '13482835675', 'imacoder@126.com', '$2a$10$3hTUSGc9W2IU9PCUxqIz2e8lf810r/z0nzIgpgy1W4FZYb/SKAkFi', '2019-12-04 17:51:49', '2020-01-04 11:15:15');
+INSERT INTO `user` VALUES (12, 'coder的自我修养', NULL, 'imacoder@126.com', '123456', '2019-12-04 17:53:25', '2019-12-04 17:53:25');
+INSERT INTO `user` VALUES (13, 'coder的自我修养', NULL, 'imacoder@126.com', '123456', '2019-12-04 17:59:22', '2019-12-04 17:59:22');
 
 -- ----------------------------
 -- Table structure for user_role
